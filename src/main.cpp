843d06ed146713c34f93d2ce1f16bf5d8a78c980
Juan Rios <juanrios@tuta.io>
2025-07-17 23:56:39 -0500

    add: png image is shown and changed when buttons are pressed; tone progresses when buttons are kept pushed

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,47 @@
+
 #include <Arduino.h>
+
+// Include the PNG decoder library
+#include <PNGdec.h>
+#include "Examples/resources/reader.h" // Image is stored here in an 8-bit array
+#include "Examples/resources/astro.h"
+#include "Examples/resources/bike.h"
+#include "Examples/resources/super.h"
+#include "Examples/resources/camera.h"
+// Include the TFT library https://github.com/Bodmer/TFT_eSPI
+#include "SPI.h"
+#include <TFT_eSPI.h>              // Hardware-specific library
 
 #define OCTAVE_OFFSET 0
 #define DEBUG false
 
+// 1. Create an array of pointers to your image data arrays.
+//    The type 'const uint8_t*' matches the 'const byte[]' from your image files.
+const uint8_t* image_data[] = {reader, astro, bike, super, camera};
+
+// 2. Create a parallel array to store the size of each image array.
+//    We use sizeof() on the original arrays here.
+const size_t image_sizes[] = {sizeof(reader), sizeof(astro), sizeof(bike)};
+
+// 3. Calculate the number of images automatically. This is more robust than hard-coding '3'.
+const int num_images = sizeof(image_data) / sizeof(image_data[0]);
+
+//int[] arr[] = [reader, astro, bike];
+
+PNG png; // PNG decoder instance
+
+#define MAX_IMAGE_WIDTH 240 // Adjust for your images
+
+int16_t xpos = 0;
+int16_t ypos = 0;
+
+TFT_eSPI tft = TFT_eSPI();         // Invoke custom library
+
 // set pin numbers
-const int buttonPin = 10;     // the number of the pushbutton pin
+const int buttonUp = 17;     // the number of the pushbutton pin
+const int buttonDown = 16;     // the number of the pushbutton pin
 const int ledPin =  LED_BUILTIN;       // the number of the LED pin
-const int speaker = 13;
+const int speaker = 15;
 int soundOn =  false;
 
 // Exact equal-tempered frequencies C3..B8 (Hz) -----------------
@@ -259,6 +294,19 @@ bool next_rtttl() {
   }
 }
 
+//=========================================v==========================================
+//                                      pngDraw
+//====================================================================================
+// This next function will be called during decoding of the png file to
+// render each image line to the TFT.  If you use a different TFT library
+// you will need to adapt this function to suit.
+// Callback function to draw pixels to the display
+void pngDraw(PNGDRAW *pDraw) {
+  uint16_t lineBuffer[MAX_IMAGE_WIDTH];
+  png.getLineAsRGB565(pDraw, lineBuffer, PNG_RGB565_BIG_ENDIAN, 0xffffffff);
+  tft.pushImage(xpos, ypos + pDraw->y, pDraw->iWidth, 1, lineBuffer);
+}
+
 //-------------------
 // MAIN PROGRAM
 //-------------------
@@ -267,24 +315,63 @@ bool play = false;
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("\n\n Starting initialisation.");
+  Serial.println("\n\n Using the PNGdec library");
+
+  // Initialise the TFT
+  tft.begin();
+  tft.fillScreen(TFT_BLACK);
+
   pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLDOWN);
+  pinMode(buttonUp, INPUT_PULLDOWN);
+  pinMode(buttonDown, INPUT_PULLDOWN);
   pinMode(speaker, OUTPUT);
+  Serial.println("\r\nInitialisation done.");
 }
+
+int i = 0;
+int tmp = -1;
+int buttonUpState = 0;
+int buttonDownState = 0;
 
 void loop(void)
 {
-  if (digitalRead(buttonPin)){
-      play = true;
+
+  buttonUpState = digitalRead(buttonUp);
+  buttonDownState = digitalRead(buttonDown);
+
+  if (buttonUpState == HIGH){
+    i += 1;
+    if (i>=num_images){
+      i = 0;
+    }
+    delay(60);
+  }else if (buttonDownState == HIGH){
+    i -= 1;
+    if (i<0){
+      i = num_images - 1;
+    }
+    delay(60);
   }
-  
-  if(play) 
-  {
+  if (tmp!=i) {
+  int16_t rc = png.openFLASH((uint8_t *)image_data[i%(sizeof(image_data)/sizeof(char *))], image_sizes[i%(sizeof(image_data)/sizeof(char *))], pngDraw);
+    if (rc == PNG_SUCCESS) {
+      Serial.println("Successfully opened png file");
+      Serial.printf("image specs: (%d x %d), %d bpp, pixel type: %d\n", png.getWidth(), png.getHeight(), png.getBpp(), png.getPixelType());
+      tft.startWrite();
+      uint32_t dt = millis();
+      rc = png.decode(NULL, 0);
+      Serial.print(millis() - dt); Serial.println("ms");
+      tft.endWrite();
+    // png.close(); // not needed for memory->memory decode
+
+  }
+
     if(!songStarts) 
     { // Start song
       digitalWrite(ledPin,HIGH); // Light the onboard LED while the song plays
       songStarts = true;
-      begin_rtttl(songs[random(sizeof(songs)/sizeof(char *))]);
+      begin_rtttl(songs[i%(sizeof(songs)/sizeof(char *))]);
     }
     if(!next_rtttl()) 
     { // Play next note
@@ -294,5 +381,7 @@ void loop(void)
       if(DEBUG) Serial.println("Done!");
       delay(500);
     }
-   }
+  tmp = i; 
+  }
+  
 }
